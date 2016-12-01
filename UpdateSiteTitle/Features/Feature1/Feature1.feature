37238b1d3d78947ad64508bfda064f0c0b0977a5
leiw2000 <leiw2000@gmail.com>
2016-12-01 16:22:49 -0500

    Add project files.

---
+++ UpdateSiteTitle/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="da91af0d-d741-4ca5-abee-f04e2155df66" description="SharePoint Add-in Feature" featureId="da91af0d-d741-4ca5-abee-f04e2155df66" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="UpdateSiteTitle Feature" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="db126294-cb1f-4bdc-a751-65fd39357605" />
+    <projectItemReference itemId="c4b808b3-fa34-4994-9476-f3689891c58a" />
+    <projectItemReference itemId="a5b055cf-6270-4d01-b2d1-f1d589e92bcd" />
+    <projectItemReference itemId="3d8f8965-120c-4732-99e7-56dc4d9471f0" />
+  </projectItems>
+</feature>
---
+++ UpdateSiteTitle/Scripts/Add-in.js
@@ -0,0 +1,81 @@
+﻿'use strict';
+
+var context;
+var hostweburl;
+var appweburl;
+var appContextSite;
+var web;
+
+$(document).ready(function () {
+    SP.SOD.executeFunc('sp.js', 'SP.ClientContext', getUrl);
+});
+function getUrl() {
+    hostweburl = getQueryStringParameter("SPHostUrl");
+    appweburl = getQueryStringParameter("SPAppWebUrl");
+    hostweburl = decodeURIComponent(hostweburl);
+    appweburl = decodeURIComponent(appweburl);
+    var scriptbase = hostweburl + "/_layouts/15/";
+    $.getScript(scriptbase + "SP.Runtime.js",
+        function () {
+            $.getScript(scriptbase + "SP.js",
+            function () { $.getScript(scriptbase + "SP.RequestExecutor.js", execOperation); }
+            );
+        }
+    );
+}
+
+function execOperation() {
+    context = new SP.ClientContext(appweburl);
+    var factory =
+        new SP.ProxyWebRequestExecutorFactory(
+            appweburl
+        );
+    context.set_webRequestExecutorFactory(factory);
+    appContextSite = new SP.AppContextSite(context, hostweburl);
+    web = appContextSite.get_web();
+    context.load(web);
+    
+    context.executeQueryAsync(onSuccess, onFail);
+}
+function onSuccess() {
+  
+    var currentTitle = web.get_title();
+    $('#texTitle').text(currentTitle);
+}
+
+// This function is executed if the above call fails
+function onFail(sender, args) {
+    alert(args.get_message());
+}
+
+function UpdateSPWebTitle() {
+   
+    var newTitle;
+    newTitle = $.trim($('#texTitleNew').val());
+    if (newTitle != "") {
+        web.set_title(newTitle);
+        web.update();
+        context.executeQueryAsync(SuccessUpdate, FailureUpdate);
+    }
+}
+
+function SuccessUpdate() {
+    $('#message').text("Update Call succeeded!");
+    alert("Update Call succeeded");
+}
+
+function FailureUpdate() {
+    alert("Update Call failed");
+}
+
+// Utility functions
+function getQueryStringParameter(paramToRetrieve) {
+    var params = document.URL.split("?")[1].split("&");
+    var strParams = "";
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] == paramToRetrieve) {
+            return singleParam[1];
+        }
+    }
+}
